6bc479f8ad56d6700eb03e1dc05f1abe6258c470
Jackeline Lima <jackelinelimap@gmail.com>
2022-07-31 20:23:15 -0300

    create uiview

--- Movies/Movies/Controller/MovieViewController.swift
+++ Movies/Movies/Controller/MovieViewController.swift
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MovieViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func loadView() {
+        super.loadView()
+    }
 }
 
---
+++ Movies/Movies/View/MovieView.swift
@@ -0,0 +1,19 @@
+//
+//  MovieView.swift
+//  Movies
+//
+//  Created by Jackeline Pires De Lima on 31/07/22.
+//
+
+import UIKit
+
+class MovieView: UIView {
+
+    init() {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
