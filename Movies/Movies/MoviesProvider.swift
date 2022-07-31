fd79a93f8f0bdb24440615a82f5c8d2fd687d91a
Jackeline Lima <jackelinelimap@gmail.com>
2022-07-31 20:12:06 -0300

    update provider

---
+++ Movies/Movies/MoviesProvider.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesProvider.swift
+//  Movies
+//
+//  Created by Jackeline Pires De Lima on 31/07/22.
+//
+
+import UIKit
+
+class MoviesProvider {
+    let apiKey = "c5afbced1d1ab3501b3e8c9baacca3be"
+}
